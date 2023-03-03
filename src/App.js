13b0bc71c75f215a23573efdfa2b1a917fa64edb
whit <whitspa64@gmail.com>
2023-03-03 12:31:07 -0500

    added App.js App.css and index.js to components directory. added manifest.json and other supporting files to public directory

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import portfolio from './components/portfolio';
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <portfolio />
+    </div>
+  );
+}
+
+export default App;
