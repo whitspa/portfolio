91bca3393e5d0c438b6bbdb8d9db734eabdd528b
whit <whitspa64@gmail.com>
2023-03-03 12:54:17 -0500

    worked on classNames and functions for navigation and project componenets

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,1 +1,33 @@
 import React, { useState } from 'react';
+
+export default function Nav() {
+    const linkStyle = { border: '1px black', padding: '5px'};
+
+    return (
+        <nav className="main-header-menu">
+          <section
+            style={{
+              display: 'flex',
+              fontFamily: 'helvetica',
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+            }}
+          >
+            <div style={linkStyle}>
+              <a href="#">About Me</a>
+            </div>
+            <div style={linkStyle}>
+              <a href="#">Portfolio</a>
+            </div>
+            <div style={linkStyle}>
+              <a href="#">Contact</a>
+            </div>
+            <div style={linkStyle}>
+              <a href="#">Resume</a>
+            </div>
+          </section>
+        </nav>
+      );
+
+}
--- src/components/project.js
+++ src/components/project.js
@@ -1,1 +1,18 @@
 import React, { useState } from 'react';
+
+export default function Project({ users }) {
+    return (
+      <div className="container">
+        <h1>Project:</h1>
+        <ul className="list-group">
+          {/* Here we use the map method to iterate through each user and return a new array of list items for each user */}
+          {Projects.map((Project) => (
+            <li className="list-project"
+            //  key={project.login.uuid}> syntax to set up the project key?
+              {`${project.title} ${project.deployed} (${project.github}) (${project.image})`}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
