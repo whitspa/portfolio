da29082aa1121efb906ce78ed0ac102d213e24c5
whit <whitspa64@gmail.com>
2023-03-08 21:54:51 -0500

    worked on basic logic and format for component pages

---
+++ temp/src/components/pages/Aboutme.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Aboutme() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hello. My name is Whit Spaulding. I am a software engineer and a
+        small business consultant who has worked with clients in retail, manufacturing,
+        education, and the military. My areas of expertise are turnaround consulting, 
+        training/coaching, and leadership development. Check out my portfolio of recent projects 
+        completed on my way to becoming a full stack web developer. I look forward to working
+        with you!
+      </p>
+    </div>
+  );
+}
