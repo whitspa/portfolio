fb452e4ce59ce6dfda2c1c24a3932a16a5eecbb7
whit <whitspa64@gmail.com>
2023-03-14 23:48:36 -0400

    added image and styling to about me page

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 export default function Aboutme() {
   return (
     <div className='section-heading container'>
       <h2 className='page-heading'>About Me</h2>
+      <Card.Img variant="top" src={process.env.PUBLIC_URL +"/images/avatar.png"} />
       <p>
         Hello. My name is Whit Spaulding. I am a software engineer and a
         small business consultant who has worked with clients in retail, manufacturing,
